ad4b5c9822a1dbb940a81b0599456edac5bc9fff
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-06-06 17:44:04 +0000

    Add coins list route implementation

---
+++ src/routes/coinsList.js
@@ -0,0 +1,31 @@
+import mockCryptoPrices from '../data/crypto-prices.json';
+
+/**
+ * Retrieves the list of coins from mock cryptocurrency data
+ * @returns {Array} List of coins with id, symbol, and name
+ */
+export function getCoinsList() {
+  // Return a copy of the mock data to prevent direct mutation
+  return mockCryptoPrices.map(coin => ({
+    id: coin.id,
+    symbol: coin.symbol,
+    name: coin.name
+  }));
+}
+
+/**
+ * Express route handler for coins list endpoint
+ * @param {Object} req Express request object
+ * @param {Object} res Express response object
+ */
+export function coinsListHandler(req, res) {
+  try {
+    const coinsList = getCoinsList();
+    res.json(coinsList);
+  } catch (error) {
+    res.status(500).json({ 
+      error: 'Unable to retrieve coins list', 
+      details: error.message 
+    });
+  }
+}
