14bc491a53c78ff96681bf1be79337af0b97941e
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-06-06 18:52:07 +0000

    Add coins service with getCoinsList function

---
+++ src/services/coinsService.ts
@@ -0,0 +1,9 @@
+import mockCoinsData from '../data/mockCoins.json';
+
+/**
+ * Retrieves the list of coins
+ * @returns {Array} List of coins with their details
+ */
+export function getCoinsList() {
+  return mockCoinsData;
+}
