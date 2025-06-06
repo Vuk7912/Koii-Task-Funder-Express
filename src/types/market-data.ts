08089677dee86a9faed4058b701e104980885b10
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-06-06 17:14:49 +0000

    Add market data type definition

---
+++ src/types/market-data.ts
@@ -0,0 +1,15 @@
+/**
+ * Represents market data for a cryptocurrency
+ */
+export interface MarketData {
+  id: string;
+  symbol: string;
+  name: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  total_volume: number;
+  price_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+}
